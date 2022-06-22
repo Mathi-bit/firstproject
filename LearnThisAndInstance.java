ef420323cd80aca0b39099a605a8bbec3266a889
unknown <mathimathi1800@gmail.com>
2022-06-22 07:58:35 +0530

    mathi

---
+++ LearnThisAndInstance.java
@@ -0,0 +1,14 @@
+package epsiode6;
+
+public class LearnThisAndInstance {
+
+ void addTwoNumber(int a,int b) { 
+	// int c; //local variable
+	 System.out.println(a+b+c);
+	 
+ }
+ public static void main(String[] args) {
+	LearnThisAndInstance ls = new LearnThisAndInstance();
+	ls.addTwoNumber(4, 5);
+}
+}
